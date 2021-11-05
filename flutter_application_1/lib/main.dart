807201de75a16a56fb2dfd45383378ee248737b0
cristianalvarezz <alvarescristian46@gmail.com>
2021-11-04 19:28:41 -0500

    mi primera aplicacion

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Nosotros escribimos
+// import 'src/app.dart';
+
+import 'package:flutter_application_1/src/app.dart';
+
+void main() {
+
+  runApp( MyApp() );
+
+}
+
+
